08f2ea1ab845a9d67435a2ec9410543c73e11a83
Dipbiswas1009dip <dipakbiswae2@gmail.com>
2024-08-25 20:45:50 +0530

    hii

--- index.js
+++ index.js
@@ -1,17 +1,20 @@
-const express = require("express")
-const data = require("./Data.json")
+const express = require("express");
+const data = require("./Data.json");
+require('dotenv').config();
 
-const app = express()
-const PORT = 3000;
+const app = express();
+const port = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
-    res.send("hello world!")
-})
+    res.send("hello world!");
+});
 
 app.get("/api", (req, res) => {
-    res.json(data)
-})
+    res.json(data);
+});
 
-app.listen(PORT, () => {
-    console.log(`app lisning is port number ${PORT}`)
-})
+app.listen(port, () => {
+    console.log(`App listening on port number ${port}`);
+}).on('error', (err) => {
+    console.error('Failed to start server:', err);
+});
